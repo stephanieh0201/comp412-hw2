21eea0670728236509e40b2081f5b5ef01374ff1
Stephanie Verlingo <stephanie.hemmer@gmail.com>
2015-06-16 10:17:01 -0500

    edited program to utilize functions and return dictionaries containing needed data

--- importcsv.py
+++ importcsv.py
@@ -1,64 +1,59 @@
 import csv
-with open('C:\Users\Stephanie\Documents\GitHub\comp412-hw2\Health_Indicators.csv', 'rb') as csvfile:
-    datafile = csv.reader(csvfile, delimiter=' ', quotechar='|')
-   
+
+def PovertyParser():
+    with open('Health_Indicators.csv', 'rb') as csvfile:
+        datafile = csv.reader(csvfile, delimiter=' ', quotechar='|')
+  
     # get headers of columns
-    for row in csvfile.readlines():
-        column = row.split(',')
-        header1 = column[0]
-    	header24 = column[23]
-        print "Data is for " + header1 + " and " + header24
-        break
-    csvfile.seek(0)
-    csvfile.next()
+        for row in csvfile.readlines():
+            column = row.split(',')
+            header1 = column[0]
+    	    header24 = column[23]
+            print "Data is for " + header1 + " and " + header24
+            break
+        csvfile.seek(0)
+        csvfile.next()
    
 
     # store data in dictionary
-    PovertyDict= {}
-    for row in csvfile:
-    	column = row.split(',')
-        data1 = column[0]
-    	data24 = column[23]
-    	PovertyDict[data1] = data24
+        PovertyDict = {}
+        for row in csvfile:
+    	    column = row.split(',')
+            data1 = column[0]
+    	    data24 = column[23]
+    	    PovertyDict[data1] = data24
 
     # print poverty data to screen    
-    print len(PovertyDict)
-    print PovertyDict.viewitems()
-    print "--------------------------------------"
+        return PovertyDict
 
 #Importing Life Expectancy data by community area
-with open('C:\Users\Stephanie\Documents\GitHub\comp412-hw2\Life_Expectancy.csv', 'rb') as csvfile:
-    datafile = csv.reader(csvfile, delimiter=' ', quotechar='|')
+def LifeExpecParser():
+    with open('Life_Expectancy.csv', 'rb') as csvfile:
+        datafile = csv.reader(csvfile, delimiter=' ', quotechar='|')
     
-    # get headers of columns
-    for row in csvfile.readlines():
-        column = row.split(',')
-        lifeheader1 = column[0]
-    	lifeheader9 = column[8]
-        print "Data is for " + lifeheader1 + " and " + lifeheader9
-        break
-    csvfile.seek(0)
-    csvfile.next()
+        # get headers of columns
+        for row in csvfile.readlines():
+            column = row.split(',')
+            lifeheader1 = column[0]
+    	    lifeheader9 = column[8]
+            print "Data is for " + lifeheader1 + " and " + lifeheader9
+            break
+        csvfile.seek(0)
+        csvfile.next()
 
-    # store data in dictionary
-    LifeDict= {}
-    for row in csvfile:
-    	column = row.split(',')
-        lifedata1 = column[0]
-    	lifedata9 = column[8]
-    	LifeDict[lifedata1] = lifedata9
-    del LifeDict['']    
-
-    # print life expectancy data to screen
-    print len(LifeDict)
-    print LifeDict.viewitems()
+        # store data in dictionary
+        LifeDict= {}
+        for row in csvfile:
+    	    column = row.split(',')
+            lifedata1 = column[0]
+    	    lifedata9 = column[8]
+    	    LifeDict[lifedata1] = lifedata9
+        del LifeDict['']    
+        return LifeDict
 
     combined = {}
-
-    for key in LifeDict:
-        x=0
-        pov = PovertyDict[x]
-        life = LifeDict[x]
-        combined[pov]=life
-        x=x+1
-    print combined.viewitems()
+    
+    print LifeDict["77"] + PovertyDict["77"]
+ 
+    #for key, value in LifeDict.items:
+     #   print value + "&" + key + ": Life-" + LifeDict[key]  + " ----  Pov-" + PovertyDict[key]  
