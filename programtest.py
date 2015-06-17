58760efb3aa1365026356f3336234d1bf577e6e5
Stephanie Verlingo <stephanie.hemmer@gmail.com>
2015-06-17 16:04:36 -0500

    Added unit test to check if the two data files exist, and added test to check that pearson coefficient is in range

--- programtest.py
+++ programtest.py
@@ -1,17 +1,18 @@
-import unittest
+import unittest, os.path
 from importcsv import *
 
 
 class ProgramTest(unittest.TestCase):
-   # def test_no_data(filename)
-    #def testFileNotExist(self):
-     #   prog=Program()
-      #  d = Program.Parser(prog, 'notfile.csv', 1,2)
-       # self.fail(d)
+    #def test_no_data(filename):
+    def testFileNotExist(self):
+        """Testing the two csv data files used in program exist."""
+        assert os.path.exists('Health_Indicators.csv')
+        assert os.path.exists('Life_Expectancy.csv')
+        
 
     def testCorrectColumnsHealth(self):
-        """Testing the Poverty Data Dictionary created by Parser() contains data
-        from the necessary columns in the data csv file."""
+        """Testing the Poverty Data Dictionary created by Parser() from Health_Indicators
+        contains data from the necessary columns in the data csv file."""
         prog=Program()
         with open('Health_Indicators.csv', 'rb') as csvfile:
                 datafile = csv.reader(csvfile, delimiter=' ', quotechar='|')
@@ -52,23 +53,6 @@ class ProgramTest(unittest.TestCase):
         self.assertItemsEqual(data3, [42.2, 70.7])
         self.assertItemsEqual(data4, [16.6, 79.8])
 
-    def testAverageRight(self):
-        """This is testing the Average() method from Program to ensure it is calulating
-        the average of the values of a dictionary correctly. Testing mean of 8,10,12 (mean=10) """
-    	prog=Program()
-        test1 ={7:8,9:10,11:12}
-        avg=Program.Average(prog, test1)
-        self.assertEqual(avg, 10)
-
-    def testPearsonRight(self):
-        """Testing to check if Pearson() calculation is working correctly. Two dictionaries 
-        same values should give a coefficient of 1"""
-    	prog=Program()
-    	test1= {1:1, 2:2, 3:3}
-    	test2= {1:1, 2:2, 3:3}
-    	output=Program.Pearson(prog, test1, test2)
-    	self.assertEqual(output, 1)
-
     def testDictsSameSize(self):
         """Testing to see if the two dictionaries created from Parser() from the two
         chicago data csv files are the same lenghth."""
@@ -86,8 +70,32 @@ class ProgramTest(unittest.TestCase):
         d1= prog.Parser('Health_Indicators.csv', 0, 23)
         d2= prog.Parser('Life_Expectancy.csv', 0, 8)
         self.assertItemsEqual(d1.keys(),d2.keys())
-        #for k in d1:
-         #   assert(k in d2)
+
+    def testAverageRight(self):
+        """Testing the Average() method from Program to ensure it is calulating
+        the average of the values of a dictionary correctly. Testing mean of 8,10,12 (mean=10) """
+        prog=Program()
+        test1 ={7:8,9:10,11:12}
+        avg=Program.Average(prog, test1)
+        self.assertEqual(avg, 10)
+
+    def testPearsonRight(self):
+        """Testing to check if Pearson() calculation is working correctly. Two dictionaries 
+        same values should give a coefficient of 1"""
+        prog=Program()
+        test1= {1:1, 2:2, 3:3}
+        test2= {1:1, 2:2, 3:3}
+        output=Program.Pearson(prog, test1, test2)
+        self.assertEqual(output, 1)
+
+    def testCoefficientInRange(self):
+        """Testing that Pearson Coefficient is in range"""
+        prog=Program()
+        d1= prog.Parser('Health_Indicators.csv', 0, 23)
+        d2= prog.Parser('Life_Expectancy.csv', 0, 8)
+        value = prog.Pearson(d1,d2)
+        self.assertGreaterEqual(value, -1)
+        self.assertLessEqual(value, 1)
 
 
 
